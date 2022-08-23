d16e7833185e4820a34b7dee8fab79536cabfd1a
Regu <regunanthan.k@tigeranalytics.com>
2022-08-23 16:15:03 +0530

    ENH:Refactor using Pipelines

--- src/HousePricePrediction/score.py
+++ src/HousePricePrediction/score.py
@@ -7,7 +7,6 @@ import numpy as np
 from logger import configure_logger
 
 from sklearn.metrics import mean_squared_error
-from sklearn.impute import SimpleImputer
 
 
 def parse_arguments():
@@ -80,38 +79,10 @@ def predict():
     log_file_path = args.log_file
     logging = configure_logger(log_file=log_file_path, console=False, log_level="INFO")
 
-    strat_test_set = pd.read_csv(os.path.join(processed_path, "strat_test_set.csv"))
+    logging.info("Downloading the Xtest and Ytest from processed path folder")
 
-    strat_test_set.drop("income_cat", axis=1, inplace=True)
-
-    # strat_test_set.drop('Unnamed: 0', axis=1, inplace=True)
-    # print(strat_test_set.columns)
-    # Loading model to compare the results
-    # final_model = pickle.load(open(os.path.join(model_path,'final_model.pkl'),'rb'))
-
-    X_test = strat_test_set.drop("median_house_value", axis=1)
-    y_test = strat_test_set["median_house_value"].copy()
-
-    imputer = SimpleImputer(strategy="median")
-
-    X_test_num = X_test.drop("ocean_proximity", axis=1)
-    imputer.fit(X_test_num)
-    X_test_prepared = imputer.transform(X_test_num)
-    X_test_prepared = pd.DataFrame(
-        X_test_prepared, columns=X_test_num.columns, index=X_test.index
-    )
-    X_test_prepared["rooms_per_household"] = (
-        X_test_prepared["total_rooms"] / X_test_prepared["households"]
-    )
-    X_test_prepared["bedrooms_per_room"] = (
-        X_test_prepared["total_bedrooms"] / X_test_prepared["total_rooms"]
-    )
-    X_test_prepared["population_per_household"] = (
-        X_test_prepared["population"] / X_test_prepared["households"]
-    )
-
-    X_test_cat = X_test[["ocean_proximity"]]
-    X_test_prepared = X_test_prepared.join(pd.get_dummies(X_test_cat, drop_first=True))
+    X_test_prepared = pd.read_csv(os.path.join(processed_path, "xtest_prepared.csv"))
+    y_test = pd.read_csv(os.path.join(processed_path, "y_test_prepared.csv"))
 
     logging.info("Unpickling the final model")
 
